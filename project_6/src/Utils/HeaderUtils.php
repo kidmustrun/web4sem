a10821e8e846cc1c7cfa5a325d67993800215259
Irina <parakerta@mail.ru>
2021-05-21 18:17:06 +0300

    for 3

---
+++ project_6/src/Utils/HeaderUtils.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Utils;
+
+class HeaderUtils
+{
+    public function checkLogin($login, $pw){
+        if($login=='admin' && $pw=='11111')
+        return true;
+        else return false;
+    }
+}
